510cf18a7bc0e9ce7cde53cc252ff47165821aad
Danila Lebediantcev <lebedyantsevds@yandex.ru>
2020-03-11 22:38:17 +0300

    Revert "Exception homework"
    This reverts commit e7209bb6

--- exceptions/src/io/humb1t/Main.java
+++ exceptions/src/io/humb1t/Main.java
@@ -13,8 +13,7 @@ public class Main {
             new LifeCycleAction().execute();
         } catch (LifeCycleActionExecutionException | AccessDeniedException e) {
             System.err.println(e.getLocalizedMessage());
-        }
-       catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
         try (FileInputStream fileInputStream = new FileInputStream(args[0])) {
@@ -27,8 +26,8 @@ public class Main {
     }
 
     public static class LifeCycleAction {
-        public void execute() throws LifeCycleActionExecutionException, AccessDeniedException, MyException {
-            throw new MyException();
+        public void execute() throws LifeCycleActionExecutionException, AccessDeniedException {
+            throw new LifeCycleActionExecutionException();
         }
     }
 
@@ -56,15 +55,5 @@ public class Main {
 
     private String returnValueOrThrowException() throws AccessDeniedException {
         return "OK";
-    }
-
-
-    //Task 1
-    //Create your own exception and add it to signature of `io.humb1t.Main.LifeCycleAction.execute
-    public static class MyException extends Exception{
-        @Override
-        public String getMessage() {
-            return "This is not-standard exception.This is my exception";
-        }
     }
 }
--- exceptions/src/io/humb1t/Parent.java
+++ exceptions/src/io/humb1t/Parent.java
@@ -1,13 +1,11 @@
 package io.humb1t;
 
-import java.nio.file.AccessDeniedException;
-
 public class Parent {
 
     public Parent() throws Main.LifeCycleActionExecutionException {
     }
 
-    public void throwOnlyOneException() throws Main.LifeCycleActionExecutionException, AccessDeniedException {
+    public void throwOnlyOneException() throws Main.LifeCycleActionExecutionException {
 
     }
 }
